33d27b4664d428b9c033b1c5589a4ad2d29dafe0
AlexeyChernov <daxter712@rambler.ru>
2015-03-23 20:14:24 +0400

    Update HeapSort.dpr

--- HeapSort.dpr
+++ HeapSort.dpr
@@ -1,22 +1,30 @@
 program Project30;
-
 {$APPTYPE CONSOLE}
 
 uses
-  SysUtils;
+SysUtils;
 
 const
-  P = 100000000;
+P = 100000000;
 
 type
-  arr = array [1..P] of Integer;
+arr = array [1..P] of Integer;
 
-procedure WriteMas;
+
+var
+a: arr;
+n, i: Integer ;
+f1, f2: TextFile;
+Time: TDateTime;
+TimeHour, TimeMin, TimeSec, TimeMil: word;
+TimeSum: double;
+
+procedure WriteMas(a: arr; n: integer);
 var i: integer;
 begin
-  for i := 1 to n do
-    write(a[i], ' ');
-  writeln;
+for i := 1 to n do
+write(a[i], ' ');
+writeln;
 end;
 
 procedure HeapSort(N: integer);
@@ -62,42 +70,30 @@ shift(L,R);
 end;
 end;
 
-var
-  mas: arr;
-  h: boolean;
-  n, i: Integer ;
-  f1, f2: TextFile;
-  Time: TDateTime;
-  TimeHour, TimeMin, TimeSec, TimeMil: word;
-  TimeSum: double;
-  name1, name2: string[80];
-
-
-
 
 begin
-    AssignFile(f1, ParamStr(1));
-    Reset(f1);
-    AssignFile(f2, ParamStr(2));
-    Rewrite(f2);
-    read(f1, n);
-    for i:= 1 to n do
-      read(f1, mas[i]);
-      time:= Now;
-      HeapSort(n);
-      Time:= Now-Time;
-      DecodeTime(Time, TimeHour, TimeMin, TimeSec, TimeMil);
-      TimeSum:= TimeMil/1000 + TimeSec + 60*TimeMin + 3600*TimeHour;
-      writeln(f2, n);
-      for i:= 1 to n-1 do
-      write(f2, mas[i], ' ');
-      writeln(f2, mas[n]);
-    Write(f2, 'Пирамидка.');
-    writeln(f2);
-    Writeln(f2, floattostr(TimeSum));
-    Writeln(f2);
-    CloseFile(f1);
-    CloseFile(f2);
+AssignFile(f1, ParamStr(1));
+Reset(f1);
+AssignFile(f2, ParamStr(2));
+Rewrite(f2);
+read(f1, n);
+for i:= 1 to n do
+read(f1, a[i]);
+time:= Now;
+HeapSort(n);
+Time:= Now-Time;
+DecodeTime(Time, TimeHour, TimeMin, TimeSec, TimeMil);
+TimeSum:= TimeMil/1000 + TimeSec + 60*TimeMin + 3600*TimeHour;
+writeln(f2, n);
+for i:= 1 to n-1 do
+write(f2, a[i], ' ');
+writeln(f2, a[n]);
+Write(f2, 'Ïèðàìèäêà.');
+writeln(f2);
+Writeln(f2, floattostr(TimeSum));
+Writeln(f2);
+CloseFile(f1);
+CloseFile(f2);
 end.
 
 
